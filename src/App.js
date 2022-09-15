527b031e6cb2171be8a7ddaa486c9316e879a0e0
MaksKozak <makskozak2400@gmail.com>
2022-09-15 14:57:06 +0300

    react-hw1-p1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Simpsons from "./component/simpsons/Simpsons";
+
+function App() {
+  return (
+      <div>
+
+        <Simpsons/>
+
+      </div>
+  );
+}
+
+export default App;
