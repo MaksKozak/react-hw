7c7c628f07c4ada91b09986ab766e27fff4bb70a
MaksKozak <makskozak2400@gmail.com>
2022-09-26 18:46:33 +0300

    hw4-routes1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import {Users} from "./components";
-
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+    Outlet
+} from "react-router-dom"
 
 function App() {
 
     return (
       <div>
-          <Users/>
+          <ul>
+              <li><Link to = {'/todos'}>Go to todos</Link></li>
+              <li><Link to = {'/albums'}>Go to albums</Link></li>
+              <li><Link to = {'/comments'}>Go to comments</Link></li>
+          </ul>
+
+          <hr/>
+          <hr/>
       </div>
   );
 }
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './user/User';
-export * from './userForm/UserForm';
-export * from './users/Users';
